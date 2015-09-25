6ad29ba2ca66637091ae664213adf44e345a0560
sirzach <zach.mcgonigal@gmail.com>
2015-09-25 18:17:51 -0400

    foosballers are asynchronous

--- app/models/team.js
+++ app/models/team.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  player1: DS.belongsTo('foosballer'),
-  player2: DS.belongsTo('foosballer')
+  player1: DS.belongsTo('foosballer', { async: true }),
+  player2: DS.belongsTo('foosballer', { async: true })
 });
