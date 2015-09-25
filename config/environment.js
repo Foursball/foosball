a7696c6ec5977decdf62be562e5d10801acb5a83
sirzach <zach.mcgonigal@gmail.com>
2015-09-25 16:32:52 -0400

    initial setup stuff

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'foosball',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://netuitivefoosball.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
