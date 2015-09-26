6d05e81edfc6df5ff6f5f2cc0a40bb8f087ebd67
sirzach <zach.mcgonigal@gmail.com>
2015-09-26 01:04:06 -0400

    games stuff

--- app/pods/foosballer/model.js
+++ app/pods/foosballer/model.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const { computed, get } = Ember;
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+
+  firstName: computed('name', function() {
+    return get(this, 'name').split(' ')[0];
+  }),
+
+  lastName: computed('name', function() {
+    return get(this, 'name').split(' ')[1];
+  })
 });
--- app/pods/games/route.js
+++ app/pods/games/route.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
 
-const { Route, get } = Ember;
+const { Route } = Ember;
 
 export default Route.extend({
   model() {
-    return get(this, 'store').findAll('game');
+    return this.store.filter('game', { orderBy: 'time' }, () => true);
   }
+
+  // afterModel(games) {
+  //   let promises = [];
+  //   const { store } = this;
+  //
+  //   games.forEach((game) => {
+  //     promises.push(store.filter('team', { id: get(game, 'team1.id') }, () => true));
+  //     promises.push(store.filter('team', { id: get(game, 'team2.id') }, () => true));
+  //   });
+  //
+  //   return RSVP.all(promises)
+  //     .then((teams) => {
+  //       let promises = [];
+  //       teams.forEach((team) => {
+  //         promises.push(store.filter('foosballer', {id: get(team, 'player1.id')}, () => true));
+  //         promises.push(store.filter('foosballer', {id: get(team, 'player2.id')}, () => true));
+  //       });
+  //
+  //       return RSVP.all(promises);
+  //     });
+  // }
 });
