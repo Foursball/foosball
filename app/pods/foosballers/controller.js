cb8ecb3ff97358cfe136848601acb82722e98142
sirzach <zach.mcgonigal@gmail.com>
2015-09-25 23:16:55 -0400

    beefing up the style and sorting players by name

---
+++ app/pods/foosballers/controller.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+const { Controller, computed, get } = Ember;
+
+export default Controller.extend({
+  sortedFoosballers: computed('model.[]', function () {
+    return get(this, 'model').sortBy('name');
+  })
+});
