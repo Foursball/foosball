e3b367b1952973196842057fc8719584732f561e
sirzach <zach.mcgonigal@gmail.com>
2015-09-29 22:20:33 -0400

    #5

--- app/pods/game/edit/controller.js
+++ app/pods/game/edit/controller.js
@@ -5,5 +5,24 @@ const { Controller, computed, get } = Ember;
 export default Controller.extend({
   cancelOrDelete: computed('model.isNotFullyCreated', function() {
     return get(this, 'model.isNotFullyCreated') ? 'Delete' : 'Cancel';
-  })
+  }),
+
+  canSave: computed('model.{team1WinsBlack,team1WinsYellow,team2WinsBlack,team2WinsYellow}', function() {
+    let game = get(this, 'model');
+    let t1wb = get(game, 'team1WinsBlack') || 0;
+    let t1wy = get(game, 'team1WinsYellow') || 0;
+    let t2wb = get(game, 'team2WinsBlack') || 0;
+    let t2wy = get(game, 'team2WinsYellow') || 0;
+    let t1wins = t1wb + t1wy;
+    let t2wins = t2wb + t2wy;
+    let totalWins = t1wins + t2wins;
+
+    if (t1wins > 2 || t2wins > 2) {
+      return false;
+    }
+
+    return totalWins === 2 || totalWins === 3;
+  }),
+
+  cannotSave: computed.not('canSave')
 });
