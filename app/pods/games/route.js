13bf7094f1065b3cd12db124d4f924e56b3c41a5
sirzach <zach.mcgonigal@gmail.com>
2015-11-29 16:29:10 -0500

    upgrading ember to 2.2

--- app/pods/games/route.js
+++ app/pods/games/route.js
@@ -1,15 +1,11 @@
 import Ember from 'ember';
 
-const { Route, get } = Ember;
+const { Route } = Ember;
 
 export default Route.extend({
   model() {
     const { store } = this;
 
-    return store
-      .query('game', {})
-      .then(() => {
-        return store.filter('game', (g) => get(g, 'time'));
-      });
+    return store.query('game', {});
   }
 });
