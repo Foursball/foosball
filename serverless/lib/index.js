c8c15311c13b7729ef0e759cacafbfaa3ed5dbb4
TheConnMan <bcconn2112@gmail.com>
2016-05-21 20:09:26 -0400

    Add map/reduce team, game, and fooser functions

--- serverless/lib/index.js
+++ serverless/lib/index.js
@@ -24,3 +24,53 @@ module.exports.getTeams = function() {
   });
   return deferred.promise;
 };
+
+module.exports.mapToArray = function(map) {
+  return Object.keys(map).map(function(key) {
+    return map[key];
+  });
+};
+
+module.exports.injectTeamsToGames = function(teamMap, games) {
+  return games.map(function(game) {
+    game.team1 = [teamMap[game.team1].player1, teamMap[game.team1].player2];
+    game.team2 = [teamMap[game.team2].player1, teamMap[game.team2].player2];
+    return game;
+  });
+};
+
+module.exports.scoreFoosers = function(fooserMap, injectedGames) {
+  injectedGames.forEach(function(game) {
+    var winningTeam = game.team1WinsBlack + game.team1WinsYellow === 2 ? 'team1' : 'team2';
+    var losingTeam = winningTeam === 'team1' ? 'team2' : 'team1';
+    game[winningTeam].forEach(function(name) {
+      fooserMap[name] = addGameToFooser(fooserMap[name], "wins");
+    });
+    game[losingTeam].forEach(function(name) {
+      fooserMap[name] = addGameToFooser(fooserMap[name], "loses");
+    });
+  });
+  return fooserMap;
+};
+
+module.exports.sortFoosers = function(scoredFoosers) {
+  return Object.keys(scoredFoosers).map(function(fooser) {
+    return scoredFoosers[fooser];
+  }).filter(function(fooser) {
+    return fooser.total !== 0;
+  }).sort(function(fooser1, fooser2) {
+    return fooser1.wins / fooser1.total < fooser2.wins / fooser2.total;
+  });
+};
+
+function addGameToFooser(fooser, field) {
+  if (fooser[field] === undefined) {
+    fooser[field] = 0;
+  }
+  if (fooser.total === undefined) {
+    fooser.total = 0;
+  }
+  fooser[field]++;
+  fooser.total++;
+  return fooser;
+}
--- serverless/ranking/handler.js
+++ serverless/ranking/handler.js
@@ -5,9 +5,13 @@ var lib = require('../lib');
 
 module.exports.handler = function(event, context, cb) {
   return Q.all([lib.getFoosers(), lib.getTeams(), lib.getGames()]).then(function(data) {
-    var foosers = data[0];
-    var teams = data[1];
-    var games = data[2];
+    var fooserMap = data[0];
+    var teamMap = data[1];
+    var games = lib.mapToArray(data[2]);
+
+    var injectedGames = lib.injectTeamsToGames(teamMap, games);
+    var scoredFoosers = lib.scoreFoosers(fooserMap, injectedGames);
+    var sortedFoosers = lib.sortFoosers(scoredFoosers);
 
     return cb(null, {
       message: 'Go Serverless! Your Lambda function executed successfully!'
