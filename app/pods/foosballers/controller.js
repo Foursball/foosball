fc4d79bdee445cb98588367417c53102b9f71654
sirzach <zach.mcgonigal@gmail.com>
2016-06-07 23:28:31 -0400

    sorting on the foosballers page

--- app/pods/foosballers/controller.js
+++ app/pods/foosballers/controller.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import computed, { equal, alias } from 'ember-computed-decorators';
 
-const { Controller, set } = Ember;
+const { Controller, set, get, setProperties } = Ember;
 
 export default Controller.extend({
   queryParams: ['sortAscending', 'sortBy', 'expandedId'],
@@ -15,20 +15,23 @@ export default Controller.extend({
   @equal('sortAscending', false) isDesc,
   @equal('sortBy', 'winPercentage') isSortWinPercentage,
   @equal('sortBy', 'winLossRatio') isSortWinLossRatio,
+  @equal('sortBy', 'foosballer.name') isSortName,
+  @equal('sortBy', 'wins') isSortWins,
   @alias('model') foosballers,
   /* jshint ignore:end */
 
   actions: {
-    sortUp() {
-      set(this, 'sortAscending', true);
-    },
-
-    sortDown() {
-      set(this, 'sortAscending', false);
-    },
-
     sortBy(prop) {
-      set(this, 'sortBy', prop);
+      let sortBy = get(this, 'sortBy');
+
+      if (sortBy === prop) {
+        this.toggleProperty('sortAscending');
+      } else {
+        setProperties(this, {
+          sortBy: prop,
+          sortAscending: false
+        });
+      }
     },
 
     expandFoosballer(foosballer) {
