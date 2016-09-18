89d9eade63538966e839615b7b2777655c72454f
sirzach <zach.mcgonigal@gmail.com>
2016-09-17 20:07:22 -0400

    responsive header

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -4,6 +4,7 @@ const { Controller, computed, inject: { service } } = Ember;
 
 export default Controller.extend({
   dialogsService: service('dialogs'),
+  FlexiLayout: service('device/layout'),
 
   foosballers: computed(function() {
     return this.store.peekAll('foosballer');
