a76e7bf34cb5f395d33a6b9e6f93cc289a460df4
sirzach <zach.mcgonigal@gmail.com>
2016-10-25 18:50:51 -0400

    styling up the players list

--- app/pods/league/model.js
+++ app/pods/league/model.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   rules: DS.hasMany('rule'),
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  foosballers: DS.hasMany('foosballer')
 });
