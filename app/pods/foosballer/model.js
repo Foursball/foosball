0dddd9aac23e630f92dc170ee078f0778e57cf17
sirzach <zach.mcgonigal@gmail.com>
2017-03-30 08:19:32 -0400

    updating to match new api configuration

--- app/pods/foosballer/model.js
+++ app/pods/foosballer/model.js
@@ -5,7 +5,8 @@ import { attr } from 'ember-computed-decorators/ember-data';
 export default DS.Model.extend({
   /* jshint ignore:start */
   @attr('string') slack,
-  @attr('string') name,
+  @attr('string') firstName,
+  @attr('string') lastName,
   @attr('string') uid,
   @attr('string') profileImageURL,
   @attr('boolean') retired,
@@ -18,27 +19,9 @@ export default DS.Model.extend({
   league: DS.belongsTo('league'),
 
   /* jshint ignore:start */
-  @computed('name')
-  /* jshint ignore:end */
-  firstName(name) {
-    let [first] = name ? name.split(' ') : ['', ''];
-    return first;
-  },
-
-  /* jshint ignore:start */
-  @computed('name')
-  /* jshint ignore:end */
-  lastName(name) {
-    let [, last] = name ? name.split(' ') : ['', ''];
-    return last;
-  },
-
-  /* jshint ignore:start */
   @computed('firstName', 'lastName')
   /* jshint ignore:end */
   displayName(firstName, lastName) {
-    let [last] = lastName;
-
-    return `${firstName} ${last}`;
+    return `${firstName} ${lastName}`;
   }
 });
--- app/pods/foosballer/serializer.js
+++ app/pods/foosballer/serializer.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   modelNameFromPayloadKey(payloadKey) {
-    if (payloadKey === 'players') {
+    if (payloadKey === 'users') {
       return this._super('foosballers');
     } else {
      return this._super('foosballer');
